e5be2148c6f25a0e6a0afb32cecff177005fd7bc
Axel Jarnigon <axel.jarnigon@gmail.com>
2021-07-07 17:06:17 +0200

    registration form working and min styled

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,42 +11,66 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('lastname')
-            ->add('firstname')
-            ->add('email')
+            ->add('lastname', null,[
+                'label' => "Nom de famille",
+                "attr" => ["class" => "mx-3"],
+                ])
+            ->add('firstname', null,[
+                'label' => "Prénom",
+                "attr" => ["class" => "mx-3"],
+                ])
+            ->add('email', null,[
+                'label' => "Email",
+                "attr" => ["class" => "mx-3"],
+                ])
             ->add('sex', ChoiceType::class, [
+                'label' => "Sexe",
                 'choices'  => [
                     'Male' => 'Male',
                     'Female' => 'Female',
                     'Other' => 'Other',
-                ]
+                ],
+                "attr" => ["class" => "mx-3"],
             ])
             ->add('birthdate', DateType::class, [
+                'label' => "Date de naissance",
+                'format' => 'dd MM yyyy',
                 'placeholder' => [
-                    'year' => 'Year', 'month' => 'Month', 'day' => 'Day',
-                ]
+                    'day' => 'Jour', 'month' => 'Mois', 'year' => 'Année',
+                ],
+                "attr" => ["class" => "mx-3"],
             ])
-            ->add('city')
-            ->add('city_code')
+            ->add('city', null,[
+                'label' => "Ville",
+                "attr" => ["class" => "mx-3"],
+                ])
+            ->add('city_code', null,[
+                'label' => "Code Postal",
+                "attr" => ["class" => "mx-3"],
+                ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Vous devez acceptez les termes.',
                     ]),
                 ],
+                "attr" => ["class" => "mx-3"],
             ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password', "class" => "mx-3"],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
@@ -58,6 +82,10 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
+            ])
+            ->add('enregistrer', SubmitType::class, [
+                "attr" => ["class" => "btn btn-gold mt-3"],
+                'row_attr' => ['class' => 'text-start']
             ])
         ;
     }
